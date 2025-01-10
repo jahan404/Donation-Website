debf6e2e958d88815492c4809427dde695ab3ca9
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-01-10 12:36:47 +0600

    donation page and button toggling done

---
+++ JS/toggling.js
@@ -0,0 +1,49 @@
+/////////////////CLICKING HISTORY BUTTON////////////////////////
+
+document.getElementById('history-button').addEventListener('click',function(){
+    //adding bg to history button
+    document.getElementById('history-button').classList.add(
+        'bg-[#B4F461]' 
+        )
+    document.getElementById('history-button').classList.remove(
+        'bg-transparent' 
+        )
+
+    //removing bg from donation button
+    document.getElementById('donation-button').classList.add(
+        'bg-transparent' 
+        )
+    document.getElementById('donation-button').classList.remove(
+        'bg-[#B4F461]' 
+        )
+
+    hideElement('donation-cards-section');
+    unhideElement('history-section')
+})
+
+
+
+
+
+
+/////////////////CLICKING DONATION BUTTON////////////////////////
+document.getElementById('donation-button').addEventListener('click',function(){
+    //adding bg to donation button
+    document.getElementById('donation-button').classList.add(
+        'bg-[#B4F461]' 
+        )
+    document.getElementById('donation-button').classList.remove(
+        'bg-transparent' 
+        )
+
+    //removing bg from history button
+    document.getElementById('history-button').classList.add(
+        'bg-transparent' 
+        )
+    document.getElementById('history-button').classList.remove(
+        'bg-[#B4F461]' 
+        )
+
+    unhideElement('donation-cards-section');
+    hideElement('history-section')
+})
---
+++ JS/utilities.js
@@ -0,0 +1,10 @@
+//hide function
+function hideElement(id){
+    document.getElementById(id).classList.add('hidden');
+}
+
+
+//unhide function
+function unhideElement(id){
+    document.getElementById(id).classList.remove('hidden');
+}
